153cf565253c5d7482d85726b246f79e9bc79e4d
romchika <romakordzadze79@gmail.com>
2024-11-01 17:03:08 +0400

    image & icons fixed

--- src/confings/CardsConfing.ts
+++ src/confings/CardsConfing.ts
@@ -11,35 +11,35 @@ export const CardsConfing : ICard[] = [
         id : 0,
         title : 'Animation',
         information  : 'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
-        img : './public/run.png',
+        img : './run.png',
         buttonText : "Get Started"
     },
     {
         id : 1,
         title : 'Design',
         information  : 'Create beautiful, usable interfaces to help shape the future of how the web looks.',
-         img : './public/gallery.png',
+         img : './gallery.png',
         buttonText : "Get Started"
     },
     {
         id : 2,
         title : 'Photography',
         information  : 'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
-         img : './public/photo.png',
+         img : './photo.png',
         buttonText : "Get Started"
     },
     {
         id : 3,
         title : 'Crypto',
         information  : 'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
-         img : './public/crypto.png',
+         img : './crypto.png',
         buttonText : "Get Started"
     },
     {
         id : 4,
         title : 'Business',
         information  : 'A step-by-step playbook to help you start, scale, and sustain your business without outside investment.',
-         img : './public/curt.png',
+         img : './curt.png',
         buttonText : "Get Started"
     }
 ]
